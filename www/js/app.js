0600beebfd83c9d72045bef4bed46d13b2e8de0d
Bobby Hines <bobby.m.hines@gmail.com>
2016-02-14 01:00:58 -0800

    Fetch spot names from api and display on main page

--- www/js/app.js
+++ www/js/app.js
@@ -1,12 +1,47 @@
 (function() {
-  var app = angular.module('starter', ['ionic']);
+  var app = angular.module('HappyScour', ['ionic']);
 
-  app.controller('MainCtrl', function ($scope, $http) {
-    $http.get('https://happy-scour-api.herokuapp.com/')
-      .success(function(res){
-        $scope.message = res.message;
-      });
+  //app.controller('MainCtrl', function ($scope, $http) {
+  //  $http.get('https://happy-scour-api.herokuapp.com/')
+  //    .success(function(res){
+  //      $scope.message = res.message;
+  //    });
+  //
+  //});
 
+  app.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/')
+
+    $stateProvider.state('home', {
+      url: '/home',
+      views: {
+        home: {
+          templateUrl: 'home.html'
+        }
+      }
+    });
+
+    $stateProvider.state('help', {
+      url: '/help',
+      views: {
+        help: {
+          templateUrl: 'help.html'
+        }
+      }
+    });
+  });
+
+  app.controller('SpotsCtrl', function ($scope, $http) {
+    $scope.spots = [];
+    $scope.getAllSpots = function() {
+      $http.get('https://happy-scour-api.herokuapp.com/spots')
+        .success(function(res) {
+          $scope.spots = res;
+        })
+        .error(function(res) {
+          alert("Error fetching all spots!");
+        });
+    }
   });
 
   app.run(function ($ionicPlatform) {
